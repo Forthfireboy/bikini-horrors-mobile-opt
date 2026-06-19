605d2a87a8058f2b5142f9df146ac4e26a0643e1
Ajwyunsx <3427560235@qq.com>
2026-06-19 19:47:51 +0800

    Add bikini-horrors songs batch 1 (173.1 MB)

---
+++ mods/bikini-horrors/songs/kaka/scripts/events.hx
@@ -0,0 +1,8 @@
+
+function postCreate() {
+    WindowUtils.winTitle = window.title = " A WILD POJA HAS APPEARED!";
+}
+
+function onSongEnd() {
+    WindowUtils.winTitle = window.title = "BIKINI HORRORS";
+}
---
+++ mods/bikini-horrors/songs/made-in-china/scripts/events.hx
@@ -0,0 +1,14 @@
+
+function postCreate() {
+    WindowUtils.winTitle = window.title = "SHOP LIKE A BILLIONAIRE";
+}
+
+function stepHit(curStep:Int) {
+    if (curStep == 1264) {
+        WindowUtils.winTitle = window.title = "NOTHING IS REAL";
+    }
+}
+
+function onSongEnd() {
+    WindowUtils.winTitle = window.title = "BIKINI HORRORS";
+}
